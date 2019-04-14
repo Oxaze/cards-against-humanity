dd6d51ed09e82f390c78182ce6e7fa530114da6c
Oxaze <julian.engst@gmail.com>
2019-04-14 20:03:34 +0200

    init 2

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// @ts-ignore
+import Vue from "vue";
+import NProgress from "vue-nprogress";
+import VueWait from "vue-wait";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+// @ts-ignore
+import App from "./App.vue";
 
+Vue.config.productionTip = false;
+Vue.use(NProgress);
+Vue.use(VueWait);
+
+const nprogress = new NProgress({ parent: "#app" });
+const wait = new VueWait({ useVuex: true });
+
+// @ts-ignore
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	nprogress,
+	wait,
+	render: h => h(App),
+}).$mount("#app");
--- src/store.js
+++ src/store.js
@@ -1,16 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import { db } from "./firebase";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
-})
+	state: {},
+	mutations: {},
+	actions: {},
+	getters: {},
+});
