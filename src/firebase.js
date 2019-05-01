c6d5ca6ae76195a28a927b01b9ccc3ab854013ca
Oxaze <julian.engst@gmail.com>
2019-05-01 16:21:14 +0200

    removed firebase functions and hostig

--- src/firebase.js
+++ src/firebase.js
@@ -11,21 +11,20 @@ const config = {
 };
 firebase.initializeApp(config);
 
-const settings = { timestampsInSnapshots: true };
-firebase.firestore().settings(settings);
+export const db = firebase.firestore();
 
-export const db = firebase
-	.firestore()
-	.enablePersistence()
-	.then(() => {
-		console.log("Initialized Cloud Firestore through Firebase.");
-	})
-	.catch(err => {
-		if (err.code === "failed-precondition") {
-			console.log("Multiple tabs open, persistence can only be enabled in one tab at a a time.");
-		} else if (err.code === "unimplemented") {
-			console.log(
-				"The current browser does not support all of the features required to enable persistence"
-			);
-		}
-	});
+// export const db = firebase
+// 	.firestore()
+// 	.enablePersistence()
+// 	.then(() => {
+// 		console.log("Initialized Cloud Firestore through Firebase.");
+// 	})
+// 	.catch(err => {
+// 		if (err.code === "failed-precondition") {
+// 			console.log("Multiple tabs open, persistence can only be enabled in one tab at a a time.");
+// 		} else if (err.code === "unimplemented") {
+// 			console.log(
+// 				"The current browser does not support all of the features required to enable persistence"
+// 			);
+// 		}
+// 	});
